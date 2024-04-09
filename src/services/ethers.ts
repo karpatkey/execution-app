54fa0a33a3798fc74e3cd0b6753dbe66ca396076
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-09 17:56:05 +0100

    fix: ethers provider from env

--- src/services/ethers.ts
+++ src/services/ethers.ts
@@ -6,48 +6,33 @@ export function getEthersProvider(blockchain: Blockchain, env: Record<string, st
   const { mev } = {
     ethereum: {
       production: {
-        mev: process?.env?.ETHEREUM_RPC_ENDPOINT_MEV,
-        // fallback: process?.env?.ETHEREUM_RPC_ENDPOINT_FALLBACK,
+        mev: env.ETHEREUM_RPC_ENDPOINT_MEV,
       },
       development: {
         mev: env.LOCAL_FORK_URL,
-        // fallback: '',
       },
       test: {
-        main: '',
-        // fallback: '',
+        mev: '',
       },
     },
     gnosis: {
       production: {
-        mev: process?.env?.ETHEREUM_RPC_ENDPOINT_MEV,
-        // main: process?.env?.GNOSIS_RPC_ENDPOINT,
-        // fallback: process?.env?.GNOSIS_RPC_ENDPOINT_FALLBACK,
+        mev: env?.GNOSIS_RPC_ENDPOINT_MEV,
       },
       development: {
         mev: env.LOCAL_FORK_URL,
-        // fallback: '',
       },
       test: {
         mev: '',
-        // test: '',
       },
     },
-  }[blockchain][(process.env.MODE || 'development') as 'development' | 'production']
+  }[blockchain][(env.MODE || 'development') as 'development' | 'production' | 'test']
 
   const network = chainId(blockchain)
   const options = [network, { staticNetwork: true }] as [
     ethers.Networkish,
     ethers.JsonRpcApiProviderOptions,
   ]
-  // console.log({ main, fallback }, options)
   const provider = new ethers.JsonRpcProvider(mev, ...options)
-  //   [mev, main, fallback]
-  //     .filter((url) => url)
-  //     .map((url, idx) => ({
-  //       priority: idx,
-  //       provider: new ethers.JsonRpcProvider(url, ...options),
-  //     })),
-  // )
   return provider
 }
