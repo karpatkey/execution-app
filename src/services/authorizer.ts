53b5a5d1082d87d5ec1a0e2700d22aea762ca8ba
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-17 18:12:20 +0100

    fix: daos for position page

--- src/services/authorizer.ts
+++ src/services/authorizer.ts
@@ -1,5 +1,6 @@
 import { getSession } from '@auth0/nextjs-auth0'
 import { IncomingMessage, ServerResponse } from 'http'
+import { slug } from 'src/utils/string'
 
 type Authorization = { error?: string; daos?: string[] }
 
@@ -10,7 +11,7 @@ function daosFromEnvVar(content?: string) {
     .filter((d) => !!d)
 }
 
-const WHITELISTED_DAOS = daosFromEnvVar(process.env.AXA_WHITELISTED_DAOS)
+const WHITELISTED_DAOS = daosFromEnvVar(process.env.AXA_WHITELISTED_DAOS).map(slug)
 const OPEN_TO_ALL_DAOS = daosFromEnvVar(process.env.AXA_OPEN_TO_ALL_DAOS)
 
 export async function authorizedDao(
@@ -24,13 +25,13 @@ export async function authorizedDao(
 
   let daos = (session.user['http://localhost:3000/roles'] ?? []) as string[]
   if (WHITELISTED_DAOS.length > 0) {
-    daos = daos.filter((d) => WHITELISTED_DAOS.includes(d.toUpperCase()))
+    daos = daos.filter((d) => WHITELISTED_DAOS.includes(slug(d)))
   }
 
   if (session) daos.push(...OPEN_TO_ALL_DAOS)
   if (daos.length == 0) return authError
 
-  if (dao && !daos.map((d) => d.toUpperCase()).includes(dao.toUpperCase())) return authError
+  if (dao && !daos.map((d) => slug(d)).includes(slug(dao))) return authError
 
   return {
     daos,
