74f63716c6840edc0c956ca62d026fa3c6f3944b
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-26 11:09:17 +0100

    feat: improve crypto icons loading

--- src/components/CryptoIcon.tsx
+++ src/components/CryptoIcon.tsx
@@ -1,17 +1,31 @@
 import Image from 'next/image'
-import { useCallback, useState } from 'react'
+import * as icons from './CryptoIcons/icons'
 
-const WITH_EXT = new Map<string, string>([
-  ['aurabal', 'webp'],
-  ['aura', 'png'],
-  ['wsteth', 'webp'],
-  ['xdai', 'webp'],
-  ['eure', 'webp'],
-  ['agve', 'webp'],
-  ['ageur', 'webp'],
-  ['gho', 'webp'],
-  ['ldo', 'webp'],
-])
+// let __FILES__: any = null
+// async function getFiles(path: any): Promise<any> {
+//   if (__FILES__) return __FILES__
+//
+//   return new Promise((resolve) => {
+//     fs.readdir(path, function(err, files) {
+//       if (err) return console.log(err)
+//       __FILES__ = files
+//
+//       resolve(files)
+//     })
+//   })
+// }
+
+// const WITH_EXT = new Map<string, string>([
+//   ['aurabal', 'webp'],
+//   ['aura', 'png'],
+//   ['wsteth', 'webp'],
+//   ['xdai', 'webp'],
+//   ['eure', 'webp'],
+//   ['agve', 'webp'],
+//   ['ageur', 'webp'],
+//   ['gho', 'webp'],
+//   ['ldo', 'webp'],
+// ])
 
 const SYMLINK = new Map<string, string>([
   ['weth', 'eth'],
@@ -28,12 +42,11 @@ const SYMLINK = new Map<string, string>([
 
 export default function CryptoIcon({ symbol, size }: { symbol: string; size?: number }) {
   let name = symbol.toLowerCase()
+
   name = SYMLINK.get(name) || name
   const s = size || 16
-  const ext = WITH_EXT.get(name) || 'svg'
-  const [src, setSrc] = useState(`/images/crypto/color/${name}.${ext}`)
-  const fallback = useCallback(() => {
-    setSrc('/images/protocols/default.svg')
-  }, [])
-  return <Image src={src} onError={fallback} width={s} height={s} alt={name} />
+
+  const defaultPath = '/images/protocols/default.svg'
+  const img = (icons as any)[name] || defaultPath
+  return <Image src={img} width={s} height={s} alt={name} priority />
 }
---
+++ src/components/CryptoIcons/icons.ts
@@ -0,0 +1,43 @@
+import aave from '../../../public/images/crypto/color/aave.svg'
+import ageur from '../../../public/images/crypto/color/ageur.webp'
+import agve from '../../../public/images/crypto/color/agve.webp'
+import aura from '../../../public/images/crypto/color/aura.png'
+import aurabal from '../../../public/images/crypto/color/aurabal.webp'
+import bal from '../../../public/images/crypto/color/bal.svg'
+import cow from '../../../public/images/crypto/color/cow.svg'
+import crv from '../../../public/images/crypto/color/crv.svg'
+import dai from '../../../public/images/crypto/color/dai.svg'
+import eth from '../../../public/images/crypto/color/eth.svg'
+import eure from '../../../public/images/crypto/color/eure.webp'
+import gho from '../../../public/images/crypto/color/gho.webp'
+import gno from '../../../public/images/crypto/color/gno.svg'
+import ldo from '../../../public/images/crypto/color/ldo.webp'
+import mkr from '../../../public/images/crypto/color/mkr.svg'
+import steth from '../../../public/images/crypto/color/steth.svg'
+import usdc from '../../../public/images/crypto/color/usdc.svg'
+import usdt from '../../../public/images/crypto/color/usdt.svg'
+import wsteth from '../../../public/images/crypto/color/wsteth.webp'
+import xdai from '../../../public/images/crypto/color/xdai.webp'
+
+export {
+  aave,
+  ageur,
+  agve,
+  aura,
+  aurabal,
+  bal,
+  cow,
+  crv,
+  dai,
+  eth,
+  eure,
+  gho,
+  gno,
+  ldo,
+  mkr,
+  steth,
+  usdc,
+  usdt,
+  wsteth,
+  xdai,
+}
