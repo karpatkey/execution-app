717f426b9928d033e6ba0e813e91109263e314bf
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-30 12:47:05 +0100

    feat: update icons

--- src/components/CryptoIcon.tsx
+++ src/components/CryptoIcon.tsx
@@ -1,32 +1,6 @@
 import Image from 'next/image'
 import * as icons from './CryptoIcons/icons'
 
-// let __FILES__: any = null
-// async function getFiles(path: any): Promise<any> {
-//   if (__FILES__) return __FILES__
-//
-//   return new Promise((resolve) => {
-//     fs.readdir(path, function(err, files) {
-//       if (err) return console.log(err)
-//       __FILES__ = files
-//
-//       resolve(files)
-//     })
-//   })
-// }
-
-// const WITH_EXT = new Map<string, string>([
-//   ['aurabal', 'webp'],
-//   ['aura', 'png'],
-//   ['wsteth', 'webp'],
-//   ['xdai', 'webp'],
-//   ['eure', 'webp'],
-//   ['agve', 'webp'],
-//   ['ageur', 'webp'],
-//   ['gho', 'webp'],
-//   ['ldo', 'webp'],
-// ])
-
 const SYMLINK = new Map<string, string>([
   ['weth', 'eth'],
   ['aave v2', 'aave'],
--- src/components/CryptoIcons/icons.ts
+++ src/components/CryptoIcons/icons.ts
@@ -1,21 +1,36 @@
 import aave from '../../../public/images/crypto/color/aave.svg'
 import ageur from '../../../public/images/crypto/color/ageur.webp'
 import agve from '../../../public/images/crypto/color/agve.webp'
+import ankr from '../../../public/images/crypto/color/ankr.svg'
 import aura from '../../../public/images/crypto/color/aura.png'
 import aurabal from '../../../public/images/crypto/color/aurabal.webp'
 import bal from '../../../public/images/crypto/color/bal.svg'
+import comp from '../../../public/images/crypto/color/comp.svg'
 import cow from '../../../public/images/crypto/color/cow.svg'
 import crv from '../../../public/images/crypto/color/crv.svg'
 import dai from '../../../public/images/crypto/color/dai.svg'
+import ens from '../../../public/images/crypto/color/ens.svg'
 import eth from '../../../public/images/crypto/color/eth.svg'
+import ethx from '../../../public/images/crypto/color/ethx.webp'
+import eura from '../../../public/images/crypto/color/eura.webp'
 import eure from '../../../public/images/crypto/color/eure.webp'
 import gho from '../../../public/images/crypto/color/gho.webp'
 import gno from '../../../public/images/crypto/color/gno.svg'
+import grt from '../../../public/images/crypto/color/grt.svg'
+import hop from '../../../public/images/crypto/color/hop.webp'
+import hopr from '../../../public/images/crypto/color/hopr.webp'
 import ldo from '../../../public/images/crypto/color/ldo.webp'
 import mkr from '../../../public/images/crypto/color/mkr.svg'
+import olas from '../../../public/images/crypto/color/olas.webp'
+import reth from '../../../public/images/crypto/color/reth.svg'
+import safe from '../../../public/images/crypto/color/safe.webp'
+import sdai from '../../../public/images/crypto/color/sdai.webp'
 import steth from '../../../public/images/crypto/color/steth.svg'
+import sushi from '../../../public/images/crypto/color/sushi.svg'
+import uni from '../../../public/images/crypto/color/uni.svg'
 import usdc from '../../../public/images/crypto/color/usdc.svg'
 import usdt from '../../../public/images/crypto/color/usdt.svg'
+import wbtc from '../../../public/images/crypto/color/wbtc.webp'
 import wsteth from '../../../public/images/crypto/color/wsteth.webp'
 import xdai from '../../../public/images/crypto/color/xdai.webp'
 
@@ -23,21 +38,36 @@ export {
   aave,
   ageur,
   agve,
+  ankr,
   aura,
   aurabal,
   bal,
+  comp,
   cow,
   crv,
   dai,
+  ens,
   eth,
+  ethx,
+  eura,
   eure,
   gho,
   gno,
+  grt,
+  hop,
+  hopr,
   ldo,
   mkr,
+  olas,
+  reth,
+  safe,
+  sdai,
   steth,
+  sushi,
+  uni,
   usdc,
   usdt,
+  wbtc,
   wsteth,
   xdai,
 }
