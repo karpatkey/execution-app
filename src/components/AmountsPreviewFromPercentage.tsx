677e22b6701661408ea5fe20d9b431d3e74f7644
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-05-02 11:59:41 +0100

    fix: add mapping for eth token out

--- src/components/AmountsPreviewFromPercentage.tsx
+++ src/components/AmountsPreviewFromPercentage.tsx
@@ -28,6 +28,7 @@ export const AmountsPreviewFromPercentage = ({
 
     if (!out && tokenOut) {
       if (tokenOut == '0xaf204776c7245bF4147c2612BF6e5972Ee483701') out = findToken('xdai')
+      if (tokenOut == '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') out = findToken('eth')
     }
 
     if (!out) return null
